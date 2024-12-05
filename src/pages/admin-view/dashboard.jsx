48eceb26a35c879f4e08467733d93d8c3ccda2a2
nuttipol <nuttipol.r@gmail.com>
2024-12-06 06:53:01 +0700

    Test embedded real time dashboard from mongoDB

--- src/pages/admin-view/dashboard.jsx
+++ src/pages/admin-view/dashboard.jsx
@@ -1,7 +1,29 @@
 function AdminDashboard() {
-    return (
+  return (
+    <div>
       <div>admin dashboard</div>
-    )
-  }
-  
-  export default AdminDashboard;
+      <div>
+        <iframe
+          style={{
+            background: '#F1F5F4',
+            border: 'none',
+            borderRadius: '2px',
+            boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+            width: '100vw',
+            height: '100vh',
+          }}
+          src="https://charts.mongodb.com/charts-art-toy-trexblackhole-dmuqhqc/embed/dashboards?id=6751762d-a4c4-474d-8d60-3dc07b36f0f3&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"
+          title="MongoDB Dashboard"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard;
+
+
+
+
+
+
